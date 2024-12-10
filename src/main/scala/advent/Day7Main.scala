56574b31f97976bb33f87fd551256a9e37ff84be
sruusk <57395313+sruusk@users.noreply.github.com>
2024-12-10 04:03:21 +0200

    Improve day7 performance

--- src/main/scala/advent/Day7Main.scala
+++ src/main/scala/advent/Day7Main.scala
@@ -16,7 +16,10 @@ object Day7Main extends App {
   // suppress informational log messages related to the inner working of Spark
   spark.sparkContext.setLogLevel("ERROR")
 
-  spark.conf.set("spark.sql.shuffle.partitions", "10")
+  spark.conf.set("spark.sql.shuffle.partitions", "100")
+
+  // Measure the time taken to execute the code
+  val start = System.nanoTime()
 
   val schema = new StructType() // This is stupid
     .add("target", StringType)
@@ -45,66 +48,80 @@ object Day7Main extends App {
     .option("delimiter", " ")
     .schema(schema)
     .csv("data/day7-input.txt")
-    .repartition(10)
-
-  df.show()
+    .repartition(100)
 
   private val part1Acc = spark.sparkContext.longAccumulator("Part 1 Accumulator")
-  private val part2Acc = new BigIntAccumulator()
-  spark.sparkContext.register(part2Acc, "BigIntAccumulator")
+  private val part2Acc = spark.sparkContext.longAccumulator("Part 2 Accumulator")
+//  private val part2Acc = new BigIntAccumulator()
+//  spark.sparkContext.register(part2Acc, "BigIntAccumulator")
 
 
   df.foreachPartition((partition: Iterator[Row]) => {
     partition.foreach(row => {
-      val target: BigInt = BigInt(row.getString(0).replace(":", ""))
+      val target: Long = row.getString(0).replace(":", "").toLong
       val values: Array[Long] = row.toSeq
         .drop(1)
         .filter(_ != null)
         .map(_.toString.toLong)
         .toArray
-      breakable {
-        var foundPart2: Boolean = false
-        for(multiply <- 0 until Math.pow(2, values.length).toInt) {
-          // Part 1
-          var part1Total: Long = 0L
-          for(x <- values.indices) {
-            // If the multiply bit is set, then we multiply the value
-            // Otherwise, we add the value
-            if((multiply & (1 << x)) != 0) part1Total *= values(x)
-            else part1Total += values(x)
-          }
-          if(part1Total == target) {
-            part1Acc.add(target.toLong)
-            if(!foundPart2) part2Acc.add(target)
-            break
-          }
-          // Part 2
-          breakable {
-            if(foundPart2) break
-            for(concat <- 0 until Math.pow(2, values.length).toInt) {
-              var part2Total: BigInt = BigInt(0)
-              for(x <- values.indices) {
-                // If the multiply bit is set, then we multiply the value
-                // If the concat bit is set, then we concatenate the value
-                // Otherwise, we add the value
-                if((multiply & (1 << x)) != 0) part2Total *= values(x)
-                else if((concat & (1 << x)) != 0) part2Total = BigInt(part2Total.toString + values(x).toString)
-                else part2Total += values(x)
-              }
-              if(part2Total == target) {
-                part2Acc.add(part2Total)
-                foundPart2 = true
-                break
-              }
-            }
-          }
-        }
-      }
+      runRow(target, values)
     })
   })
 
+  private def runRow(target: Long, values: Array[Long]): Unit = {
+    val power = Math.pow(2, values.length).toInt
+    val stringValues = values.map(_.toString)
+    var foundPart2: Boolean = false
+    var part1Total: Long = 0L
+    var part2Total: Long = 0L
+
+    def runPart2(multiply: Int): Unit = {
+      if (foundPart2) return
+      for (concat <- 0 until power) {
+        part2Total = 0L
+        breakable {
+          for (x <- values.indices) {
+            // If the multiply bit is set, then we multiply the value
+            // If the concat bit is set, then we concatenate the value
+            // Otherwise, we add the value
+            if ((multiply & (1 << x)) != 0) part2Total *= values(x)
+            else if ((concat & (1 << x)) != 0) part2Total = part2Total * Math.pow(10, stringValues(x).length).toLong + values(x)
+            else part2Total += values(x)
+            if(part2Total > target) break
+          }
+        }
+        if (part2Total == target) {
+          part2Acc.add(part2Total)
+          foundPart2 = true
+          return
+        }
+      }
+    }
+
+    for (multiply <- 0 until power) {
+      // Part 1
+      part1Total = 0L
+      for (x <- values.indices) {
+        // If the multiply bit is set, then we multiply the value
+        // Otherwise, we add the value
+        if ((multiply & (1 << x)) != 0) part1Total *= values(x)
+        else part1Total += values(x)
+      }
+      if (part1Total == target) {
+        part1Acc.add(target)
+        if (!foundPart2) part2Acc.add(target)
+        return
+      }
+      // Part 2
+      runPart2(multiply)
+    }
+  }
+
   println(s"Part1 total calibration result: ${part1Acc.value}")
   println(s"Part2 total calibration result: ${part2Acc.value}")
+
+  val end = System.nanoTime()
+  println(s"Time taken: ${(end - start) / 1000000}ms")
 
 
   // Stop the Spark session
